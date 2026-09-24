a263fda1a92046f0629010886a3c81869b95a74b
Zelenskyi_Bohdan <zel.bohd@gmail.com>
2026-09-24 22:23:55 +0300

    added queries for the Portfolio section

--- src/js/api.js
+++ src/js/api.js
@@ -14,3 +14,25 @@ export async function getFeedbacks(limit = 10, page = 1) {
 
   return response.data;
 } 
+
+
+// ------------------------------------------------------------------------------------------------
+// Portfolio
+// ------------------------------------------------------------------------------------------------
+
+
+const BASE_URL = 'https://wedding-photographer.b.goit.study/api';
+
+export async function fetchCategories() {
+  const response = await fetch(`${BASE_URL}/categories`);
+  if (!response.ok) throw new Error(`Помилка HTTP: ${response.status}`);
+  return await response.json();
+}
+
+export async function fetchPhotos(categoryId, page = 1, limit = 9) {
+  const categoryParam = categoryId !== 'all' ? `&categoryId=${categoryId}` : '';
+  const url = `${BASE_URL}/wedding-photos?page=${page}&limit=${limit}${categoryParam}`;
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`Помилка HTTP: ${response.status}`);
+  return await response.json();
+}
