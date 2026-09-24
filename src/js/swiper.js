1ba1cf91307ac9c0785fe3cdf5824a614ca01c54
Zelenskyi_Bohdan <zel.bohd@gmail.com>
2026-09-24 11:41:14 +0300

    imported swiper.js into main.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -6,33 +6,35 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
 
-const swiper = new Swiper('.swiper', {
+export function initSwiper() {
+  new Swiper('.swiper', {
     modules: [Navigation, Pagination],
 
-    slidesPerView: 1, 
-  spaceBetween: 16,
+    slidesPerView: 1,
+    spaceBetween: 16,
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    addIcons: false,
-  },
-
-  breakpoints: {
-    // Tablet (768px+)
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 24,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
-    // Desktop (1440px+)
-    1440: {
-      slidesPerView: 3,
-      spaceBetween: 24,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      addIcons: false,
     },
-  },
-    });
+
+    breakpoints: {
+      // Tablet (768px+)
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      // Desktop (1440px+)
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
+}
